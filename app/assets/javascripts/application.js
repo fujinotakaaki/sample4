bb37dc2fae7bea9d32805f3e6b2c8f9aed3a1e5c
藤野貴明 <fujinotakaaki@fujinotakaakinoMacBook-Pro.local>
2020-02-12 19:06:13 +0900

    remove lifegame

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,42 +10,18 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require life_game_map
-//= require life_game
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
 //= require_tree .
 
-// ライフゲームに関する簡単な設定変更はここで行う
-// 細かい設定は下記リンクをいじる
-// app/assets/javascripts/life_game.js
-// 秒数カウント用変数
-var generation;
-// ライフゲームを格納する変数
-var life_game;
-// 呼び出す盤面（デフォルトはグライダー銃）
-const pattern = ["Clock1", "Clock2", "GliderGun", "Pentadecathlon", "Pinwhell", "Pulsar", "Pulsarx2", "Galaxy",
-'HertzOscillator', 'Galaxyx2', 'Galaxyx2x', 'Coe\'s_p8', 'Achim\'s_p8', 'Achim\'s_p144', '60P312'];
-const name = pattern[pattern.length-1];
-// const name = pattern[2];
 
-// １単位時間の設定
-const interval = 80;
-// オプション（上下左右反転、生死セル表示の変更可。回転はまだ）
-var options; //オプションなしがデフォルト（コメントアウト解除でオプションの設定が可能）
-options = { alive:'■', dead:'□', upside_down:false, flip:false, rotate:0 };
-// options = { alive:'■', flip:true };
-// options = { alive:'生', dead:'　', upside_down:false, flip:false, rotate:0 };
 
 // 繰り返し処理の中身
 function showPassage() {
-  if ( generation != 0 ) { life_game.UpDate; }
-  else {
-    life_game = new LifeGame( name, options );
-  }
+  const name = "ppp";
   var msg = "第" + generation + "世代（" + name + "）";   // 表示文作成
-  var msg1 = life_game.GetMap;
+  var msg1 = "Success";
   document.getElementById("PassageArea").innerHTML = msg; // 表示更新
   document.getElementById("PassageArea2").innerHTML = msg1; // 表示更新
   generation++;   // カウントアップ
@@ -54,7 +30,7 @@ function showPassage() {
 // 繰り返し処理の開始
 function startShowing() {
   generation = 0;   // カウンタのリセット
-  PassageID = setInterval('showPassage()', interval);   // タイマーをセット(1000ms間隔)
+  PassageID = setInterval('showPassage()', 1000);   // タイマーをセット(1000ms間隔)
   document.getElementById("startcount").disabled = true;   // 開始ボタンの無効化
 }
 
